06d6d0457a02896dd9515dbd7bc14c78acfb6259
Jh Zhang <jzhang91@u.rochester.edu>
2019-10-07 16:32:33 -0400

    main construction

--- core.py
+++ core.py
@@ -1,9 +1,16 @@
 import selenium
+from selenium import webdriver
 import os
 
-def batch_dl(query_word, target_dir, driver_dir, number_images):
+def get_urls(query_word, wd, number_images, humanity_interval):
+	print(query_word)
+
+def batch_dl_single_query(query_word, target_dir, driver_dir, number_images):
 	target_folder = os.path.join(target_dir, '_'.join(query_word.lower().split(' ')))
 	if not os.path.exists(target_folder):
 		os.makedirs(target_folder)
 
-batch_dl("dog food", "./images", "/", 5)
+	with webdriver.Chrome(executable_path = driver_dir) as wd:
+		url_repo = get_urls(query_word, wd, number_images, 0.5)
+
+batch_dl_single_query("dog food", "./images", "chromedriver.exe", 5)
